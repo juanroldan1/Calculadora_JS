457f36421ac9b887beeb0ddf853a37188368dfb0
Ju4nd-r0ld4n <juansanrol@unisabana.edu.co>
2025-08-13 17:40:50 -0500

    tecla_mas_menos

---
+++ funciones.js
@@ -0,0 +1,28 @@
+//Elementos
+document.getElementsByClassName(tecla_0);
+document.getElementsByClassName(tecla_1);
+document.getElementsByClassName(tecla_2);
+document.getElementsByClassName(tecla_3);
+document.getElementsByClassName(tecla_4);
+document.getElementsByClassName(tecla_5);
+document.getElementsByClassName(tecla_6);
+document.getElementsByClassName(tecla_7);
+document.getElementsByClassName(tecla_8);
+document.getElementsByClassName(tecla_9);
+document.getElementsByClassName(tecla_AC);
+document.getElementsByClassName(tecla_mas_menos);
+document.getElementsByClassName(tecla_7);
+document.getElementsByClassName(tecla_8);
+
+
+
+class Funciones {
+    mostrar(numero){
+
+    }
+    
+    sumar(params) {
+        
+    }
+}
+    
